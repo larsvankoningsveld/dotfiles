916a426fd3ca212956d2d59af4c512e0be8c70c5
Lars van Koningsveld <lars@violet88.nl>
2025-04-16 15:04:42 +0200

    Add harpoon add surround keymaps add noice

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = true },
   { "rcarriga/nvim-notify", enabled = false },
   {
     "neovim/nvim-lspconfig",
