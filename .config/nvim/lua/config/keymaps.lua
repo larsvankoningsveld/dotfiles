09e86a9fdc4189d7fba6fc195c084f9442f46d96
Lars van Koningsveld <lars@violet88.nl>
2025-04-26 11:58:31 +0200

    Add telescope and treesitter

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+local map = vim.keymap.set
+
+-- General
+map('n', '<leader>pv', vim.cmd.Ex, { desc= 'Netrw'})
+
+-- Telescope
+local builtin = require('telescope.builtin')
+map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
