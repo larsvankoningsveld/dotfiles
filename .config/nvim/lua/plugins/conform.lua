148bb276eb63d68a36d0646d3718b773a5779b86
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-05-24 20:23:27 +0200

    Add blade correctly

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,1 +1,17 @@
-return {}
+print("✅ conform.lua loaded")
+
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters = {
+      ["blade-formatter"] = {
+        command = "/usr/local/bin/blade-formatter",
+        args = { "--stdin" },
+        stdin = true,
+      },
+    },
+    formatters_by_ft = {
+      blade = { "blade-formatter" },
+    },
+  },
+}
